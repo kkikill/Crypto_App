f9ed2d0ca4ec50cca1bcace8a072701084c359a6
kirill <leo.miki2013@gmail.com>
2025-10-18 03:08:49 +0300

    start of project

---
+++ app/src/main/java/com/example/cryptoapp/activity/IntroActivity.java
@@ -0,0 +1,29 @@
+package com.example.cryptoapp.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.cryptoapp.R;
+import com.example.cryptoapp.databinding.ActivityIntroBinding;
+
+public class IntroActivity extends AppCompatActivity {
+    ActivityIntroBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding=ActivityIntroBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.starBtn.setOnClickListener(v -> startActivity(new Intent(IntroActivity.this, MainActivity.class)));
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+    }
+}
